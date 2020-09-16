bbc0c6bbb01bb462aa01ca5af31e5f91b0856d10
Asheer Rizvi <asheer.vil@gmail.com>
2020-09-16 12:23:24 +0530

    Updates

---
+++ precache-manifest.749220a1063583aba345b0192d1fbce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566d5cb33dea751e3e41b8412e82c8c2",
+    "url": "/tic-tac-toe-minimax/index.html"
+  },
+  {
+    "revision": "eeca919709cf35f9e60f",
+    "url": "/tic-tac-toe-minimax/static/css/main.70e01bd6.chunk.css"
+  },
+  {
+    "revision": "193436f4b6fa95819546",
+    "url": "/tic-tac-toe-minimax/static/js/2.f0eb8fb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-minimax/static/js/2.f0eb8fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeca919709cf35f9e60f",
+    "url": "/tic-tac-toe-minimax/static/js/main.020d60ca.chunk.js"
+  },
+  {
+    "revision": "eef041097591ac616622",
+    "url": "/tic-tac-toe-minimax/static/js/runtime-main.d44d4e9b.js"
+  }
+]);
